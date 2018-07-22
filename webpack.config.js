3d90dc5be5725e597d311a21b3827cd16eefd3ce
rkm-dev <k.m_rahul@yahoo.com>
2018-07-23 03:40:00 +0530

    Add error Modal

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,8 @@ module.exports = {
          Examples: 'app/components/Examples.jsx',
          WeatherForm: 'app/components/WeatherForm.jsx',
          WeatherMessage: 'app/components/WeatherMessage.jsx',
-         OpenWeatherMap: 'app/api/OpenWeatherMap.jsx'
+         ErrorModal: 'app/components/ErrorModal.jsx',
+         OpenWeatherMap: 'app/api/OpenWeatherMap.jsx' 
       },
       extensions: ['', '.js', '.jsx']
    },
